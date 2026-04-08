167251b37e170b804029dd2b8e27b03a0952008c
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2026-04-08 20:10:54 +0300

    stable

--- scripts/manage.py
+++ scripts/manage.py
@@ -138,12 +138,12 @@ PLATFORM = platform.system()
 ARCH = platform.machine()
 PY_VER_MINOR = sys.version_info.minor
 
-STABLE_BUILD = getenv("STABLE_BUILD", False)
+STABLE_BUILD = getenv("STABLE_BUILD", True)
 if STABLE_BUILD:
     # known to build and work without errors, 100% tests pass
-    LLAMACPP_VERSION = "b8522"
+    LLAMACPP_VERSION = "b8705"
     WHISPERCPP_VERSION = "v1.8.4"
-    SDCPP_VERSION = "master-537-545fac4"
+    SDCPP_VERSION = "master-558-8afbeb6"
     SQLITEVECTOR_VERSION = "0.9.93"
 else:
     # experimental bleeding-edge builds ` = ""` means get latest
